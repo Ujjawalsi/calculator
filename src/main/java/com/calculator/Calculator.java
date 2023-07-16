b76ce51012daa161ba72dcfcf31ea65126e1cdc1
Ujjawal Singh <ujjawals97@gmail.com>
2023-07-16 17:03:11 +0530

    "Final changes"

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -17,9 +17,9 @@ public class Calculator {
         return num1 * num2;
     }
 
-    public double divide(int num1, int num2) throws IllegalAccessException {
+    public double divide(int num1, int num2) {
         if (num2==0){
-            throw new IllegalAccessException("Given Number Cannot divide by Zero");
+            throw new ArithmeticException("Given Number Cannot divide by Zero");
         }
 
         return  num1/num2;
